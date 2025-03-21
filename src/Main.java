90147508a6c84961c8fa63224146abaa4048abed
6002glo <yushuka.safa@strathmore.edu>
2025-03-21 16:18:16 +0300

    first test

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Print a welcome message
+        System.out.printf("Hello and welcome!\n");
 
+        int sum = 0; // Initialize a variable to hold the sum
+
+        // Loop from 1 to 5
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
+            sum += i; // Add the current value of i to sum
         }
+
+        // Print the total sum
+        System.out.println("The sum of numbers from 1 to 5 is: " + sum);
     }
 }
